ce6d75a0db697895ae0f50623c20459dc83b61d3
tsotne123 <tsotne@profairgames.com>
2023-01-15 14:51:27 +0400

    made header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.css"
+import Hamburger from 'hamburger-react'
 
 const Header = () => {
+
+    const [isOpen, setOpen] = useState(false)
+    const [isActive, setActive] = useState(false);
+
+    const hamburgerMenuButtonHandler = () => {
+        setActive(!isActive);
+    }
+
+
   return (
     <nav className='header'>
         <h1 className='brand-name'> <span>Pet</span>Lover</h1>
-        <div className='header-menu'>
+        <div className={isActive ? "header-menu header-menu-show" : "header-menu"}>
             <ul>
                 <li className='header-item '> About </li>
                 <li className='header-item '> Service </li>
@@ -13,7 +23,10 @@ const Header = () => {
                 <li className='header-item '> Booking </li>
                 <li className='header-item '> Contact </li>
             </ul>
-            <button className='header-button'> Get Quote </button>
+            <button type='button' className='header-button'> Get Quote </button>
+        </div>
+        <div onClick={hamburgerMenuButtonHandler} className="hamburger-menu-button">
+            <Hamburger toggled={isOpen} toggle={setOpen} />
         </div>
     </nav>
   )
